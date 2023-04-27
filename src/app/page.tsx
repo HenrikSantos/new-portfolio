c06af64b332e20372b0bcbd9be94343346f93823
Henrik Santos <henrik.ruan4@gmail.com>
2023-04-27 12:19:31 -0300

    feat: development navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>faspifja</p>
-    </main>
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>faspifja</p>
+      </main>
+    </>
   );
 }
