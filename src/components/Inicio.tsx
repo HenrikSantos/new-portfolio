125c105c2de3edbca90d6f054abf434301faca46
Henrik Santos <henrik.ruan4@gmail.com>
2023-05-27 22:11:26 -0300

    feat: stack

--- src/components/Inicio.tsx
+++ src/components/Inicio.tsx
@@ -1,6 +1,6 @@
 export default function Inicio() {
 	return (
-		<section className='primary' id='inicio'>
+		<section className='my-20' id='inicio'>
 			<h1 className='text-4xl text-center font-thin'>BEM-VINDO AO MEU</h1>
 			<h1 className='text-[4.2rem] emphasis mx-auto text-center my-4 italic underline font-medium'>
 				PORTFÓLIO
--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,5 +1,38 @@
 export default function Stack() {
 	return (
-		<div>Stack</div>
+		<section id='stack' className='mt-5 mb-7 mx-8 flex justify-center flex-col lg:w-8/12'>
+			<h1 className='mb-1'><span className='text-xl'>Stack</span>:</h1>
+			<ul className='flex flex-col space-y-1 font-light text-sm'>
+				<li>
+					- <span className='underline text-lg'>Front End</span>: HTML, CSS, Javascript, TypeScript, React, Redux Tailwind e Bootstrap;
+				</li>
+				<li>
+					- <span className='underline text-lg'>Back End</span>: Python, Node, Express, MySql, MongoDB, Mongoose e Sequelize;
+				</li>
+				<li>
+					- <span className='underline text-lg'>QA</span>: Jest, Vitest, Mocha, Chai, Sinon e Pytest;
+				</li>
+				<li>
+					- <span className='underline text-lg'>Arquiteturas</span>:  RESTful, TDD e SOLID;
+				</li>
+				<li>
+					- <span className='underline text-lg'>Outros</span>: Git, Docker e Eslint;
+				</li>
+			</ul>
+			{/* <ul className='font-light text-sm'>
+				<li className='text font-normal'>
+					✔️ - <span className='underline'>Links</span>:
+				</li>
+				<li>
+					- <a className='hover:cursor-pointer hover:underline' href="https://github.com/HenrikSantos">GitHub</a>
+				</li>
+				<li>
+					- <a className='hover:cursor-pointer hover:underline' href="https://henriksantos.github.io/">Site Portfólio</a>
+				</li>
+				<li>
+					- < a className='hover:cursor-pointer hover:underline' href="https://www.youtube.com/@gimmicodando730">Canal no YT</a>
+				</li>
+			</ul> */}
+		</section>
 	);
 }
